f501460e44b8ca47ad69e0dc7f6d6b28e35c9a1d
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-01-25 18:43:25 +0300

    feat: added thunk

--- src/redux/features/books/booksSlice.js
+++ src/redux/features/books/booksSlice.js
@@ -1,2 +1,14 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+
+const initialState = {
+  books: [],
+};
+
+export const getBooks = createAsyncThunk("getBooks", async () => {});
+
+export const booksSlice = createSlice({
+  name: "books",
+  initialState,
+  reducers: {},
+});
