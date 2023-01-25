7081cad4ff42a45137ecefcacd2e037c77a79c6a
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-01-25 18:27:28 +0300

    style: edited header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,22 @@
 import React from "react";
 import "./Header.css";
 
-function Header() {
-  return <div>Header</div>;
+function Header({ search, setSearch }) {
+  return (
+    <header>
+      <div>
+        <h1>Search Books App</h1>
+        <form>
+          <input
+            type="text"
+            placeholder="Search a book"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </form>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
