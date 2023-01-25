92ea384e058cd951b0685d8c4f247d205d01dae6
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-01-25 18:26:35 +0300

    style: edited footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Footer.css";
 
 function Footer() {
-  return <div>Footer</div>;
+  return <footer>Made with by ❤️ Neşathan Öztürk</footer>;
 }
 
 export default Footer;
