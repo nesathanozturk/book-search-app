1c37771e9bcd5131b65f54e0c1cf583e4c925147
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-01-25 19:29:42 +0300

    feat: added books in main

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import Footer from "./components/Footer/Footer";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,44 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
-function Main({ books }) {
-  const renderedBooks = books.map((book, index) => <div key={index}></div>);
+function Main() {
+  const books = useSelector((state) => state.booksReducer.books);
 
-  return { renderedBooks };
+  return (
+    <div className="m-auto w-11/12 mt-5 min-h-[55vh] grid grid-cols-3 gap-3">
+      {books && books.length > 0 ? (
+        books.map((book) => {
+          return (
+            book.volumeInfo.imageLinks &&
+            book.volumeInfo.authors && (
+              <div
+                className="w-80 m-5 text-center bg-slate-200 p-3 rounded text-black inline-block"
+                key={book.id}
+              >
+                <img
+                  className="w-11/12 h-52 ml-auto mr-auto object-contain pb-4"
+                  alt="ımagesLive"
+                  src={book.volumeInfo.imageLinks.smallThumbnail}
+                />
+                <h5 className="mt-4 text-base">{book.volumeInfo.title}</h5>
+                <h6 className="mt-0 mb-4 inline-block p-1 pt-6 text-base">
+                  {book.volumeInfo.authors[0]}
+                </h6>
+                <br />
+                <button className="inline-block hover:text-red-300 text-black">
+                  Preview
+                </button>
+              </div>
+            )
+          );
+        })
+      ) : (
+        <div className="text-center text-4xl ml-[550px] flex justify-center">
+          <div>Find book</div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Main;
