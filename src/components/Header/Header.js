e3749324b34883dd20668dbf151793aca90de549
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-01-25 19:05:31 +0300

    feat: imported bookSlice

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { getBooks } from "../../redux/features/books/booksSlice";
 import "./Header.css";
 
-function Header({ search, setSearch }) {
+function Header() {
+  const [search, setSearch] = useState("");
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    if (e) e.preventDefault();
+    dispatch(getBooks({ name: search }));
+  };
+
   return (
     <header>
       <div>
         <h1>Search Books App</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input
             type="text"
             placeholder="Search a book"
--- src/redux/features/books/booksSlice.js
+++ src/redux/features/books/booksSlice.js
@@ -2,13 +2,29 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const initialState = {
-  books: [],
+  books: [], // state
+  search: "",
+  length: 0,
 };
 
-export const getBooks = createAsyncThunk("getBooks", async () => {});
+export const getBooks = createAsyncThunk("getBooks", async () => {
+  const res = await axios.get(
+    "https://www.googleapis.com/books/v1/volumes?q=intitle:${search.name}&printType=books&orderBy=newest&maxResults=35"
+  );
+  return res.data.items;
+});
 
 export const booksSlice = createSlice({
   name: "books",
   initialState,
   reducers: {},
+
+  extraReducers: (builder) => {
+    builder.addCase(getBooks.fulfilled, (state, action) => {
+      state.books = action.payload;
+      state.books.length = action.payload.length;
+    });
+  },
 });
+
+export default booksSlice.reducer;
