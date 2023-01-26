741f8c25a840722bd19342506a583a708c53c4c6
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-01-26 15:10:15 +0300

    feat: added animatecss

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
 import "./App.css";
+import "animate.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App animate__animated animate__fadeIn">
       <Header />
       <Main />
       <Footer />
